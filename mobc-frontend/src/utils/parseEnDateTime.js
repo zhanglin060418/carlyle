8cde840e0a3d698f71eb07ea55cf59284456d549
zhanglin <zhanglin060418@163.com>
2023-08-23 02:08:31 +0800

    修改token时间

--- mobc-frontend/src/utils/parseEnDateTime.js
+++ mobc-frontend/src/utils/parseEnDateTime.js
@@ -1,26 +1,9 @@
 export function parseEnDateTime(val) {
-  if (val == null)
-    return
-  const now = new Date(val)
-  var year = now.getFullYear();       //年
-  var month = now.getMonth() + 1;     //月
-  var day = now.getDate();            //日
-  var hh = now.getHours();            //时
-  var mm = now.getMinutes();          //分
-  var ss = now.getSeconds();
-  var clock = '';
-  if(day < 10)
-    clock += "0";
-  clock += day + "/";
-  if(month < 10)
-    clock += "0";
-  clock += month + "/";
-  clock += year + " "
-  if(hh < 10)
-    clock += "0";
-  clock += hh + ":";
-  if (mm < 10) clock += '0';
-  clock += mm + ":";
-  clock += ss;
-  return(clock);
+  //let now = new Date(val)
+  let year = val.substring(0,4)    //年
+  let month = val.substring(5,7);     //月
+  let day = val.substring(8,10);           //日
+  let hhmmss = val.substring(val.length-8,val.length); //时分秒
+  let clock = day+"/"+month+"/"+year+" "+hhmmss;
+  return clock;
 }
